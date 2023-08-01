bd5e40256f4da608e47d7d5a75a90eec2786ce71
Niko <niko@MacBook-Pro-von-Niko.local>
2023-08-01 15:07:21 +0200

    sasa changes

--- comp_physics/main.py
+++ comp_physics/main.py
@@ -1,13 +1,14 @@
 from typing import List
-
+from random import randint as rand
 import matplotlib.pyplot as plt
-import pygame
+
 
 from src.planet import get_planets, Planet, WIDTH, HEIGHT
 
 
-pygame.init()
-WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+def task1(planets: List[Planet]):
+    for planet in planets:
+        pass
 
 
 def task2(planets: List[Planet]):
@@ -18,40 +19,112 @@ def task2(planets: List[Planet]):
 
 def task4(planets: List[Planet]):
     plt.axes(projection='3d')
+    #planets = planets [:4]
     for planet in planets:
         planet.create_orbit_z()
     plt.show()
 
 
-def pygame_func():
+def task3():
+
+    import pygame
+    pygame.init()
+    WIN = pygame.display.set_mode((WIDTH,HEIGHT))
     run = True
     clock = pygame.time.Clock()
 
     planets: List[Planet] = get_planets()
+    frames = 0
+
+    if input("first four?")[0] == 'y':
+        planets = planets[:4]
+        for planet in planets:
+            planet.SCALE = 150/Planet.AU
+    else:
+        planets = planets[4:]
 
     while run:
         clock.tick(60)
-        WIN.fill((0,0,0))
         pygame.display.update()
+        frames += 1 #increases by one every frame, counting them
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
         
-        # planets = planets[:1] # get just mercury
+        #planets = planets[3:] # get just mercury
+
         for planet in planets:
-            planet.DRAW(WIN)
+            if frames % 2 == 0:
+                WIN.fill((0,0,0))
+            togethercoords, locations = planet.create_orbit()
+            #print (planet.name, locations[frames], "\n")
+            planet.DRAW(WIN,locations[frames])
+            pygame.draw.lines(WIN, planet.color, False, togethercoords, 2)
 
         pygame.display.update()
+
     pygame.quit()
 
+
+
+def task5():
+    planets: List[Planet] = get_planets()
+    planets = planets[:1]
+    for planet in planets:
+        Planet.time_at_certain_angles(planet)
+
+
+def task6():
+    import pygame
+    pygame.init()
+    WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+    WIN.fill((255,255,255))
+    run = True
+    clock = pygame.time.Clock()
+
+    planets: List[Planet] = get_planets()
+
+    planets = [planets[1],planets[2]]
+    frames = 0
+
+    while run and frames < planets[1].ten_rotations_frames():
+        #print (planets[1].ten_rotations_frames())
+        clock.tick(60)
+        pygame.display.update()
+        frames += 1 #increases by one every frame, counting them
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        
+        togethercoords, locations = planets[0].create_orbit()
+        pygame.draw.lines(WIN, planets[0].color, False, togethercoords, 2)
+        togethercoords, locations = planets[1].create_orbit()
+        pygame.draw.lines(WIN, planets[1].color, False, togethercoords, 2)
+        try:
+            if frames % round(planets[1].calc_frame_mod()) == 0:
+                Planet.match_up_locations(planets[0],planets[1],WIN,frames)
+                #print(round(planets[1].calc_frame_mod()))
+        except ZeroDivisionError:
+            Planet.match_up_locations(planets[0],planets[1],WIN,frames)
+
+        pygame.display.update()
+
+    pygame.quit()
 
 
 def main():
     planets: List[Planet] = get_planets()
     task2(planets)
-    task4(planets)
+    #task4(planets)
+    #task3()
+    #task5()
+    # task6()
 
 
 if __name__ == '__main__':
     main()
+    #Planet.match_up_locations(planets[0],planets[1])
+
+    
--- comp_physics/src/planet.py
+++ comp_physics/src/planet.py
@@ -1,20 +1,21 @@
 from typing import List
 import math
+from random import randint as rand
 
 import matplotlib.pyplot as plt
 import pygame
 
-from src.data import data
+from .data import data
 
-WIDTH, HEIGHT = 600,600
+WIDTH, HEIGHT = 700,700
 
 class Planet:
     
     AU = 149.6e6 * 1000
-    SCALE = 150 / AU
+    SCALE = 10 / AU
     TIMESTEP = 3600 * 24
 
-    def __init__(self, name, orbital_period, semi_major_axis, mass, eccentricity, inclination,semi_minor_axis,x,y):
+    def __init__(self, name, orbital_period, semi_major_axis, mass, eccentricity, inclination,semi_minor_axis,x,y,color):
         self.name = name
         self.orbital_period = orbital_period
         self.semi_major_axis = semi_major_axis
@@ -22,8 +23,9 @@ class Planet:
         self.eccentricity = eccentricity
         self.inclination = inclination
         self.semi_minor_axis = semi_minor_axis
-        self.color = (255,0,0)
+        self.color = color
         self.orbit = []
+        self.first_four_planets = True
 
         self.x = x
         self.y = y
@@ -31,35 +33,128 @@ class Planet:
         self.y_vel = 0
         self.x_vel = 0
 
-    def DRAW(self, win):
-        x = self.x * self.SCALE + WIDTH / 2
+    def findr(self, angle):
+        return ((self.semi_major_axis*(1-(self.eccentricity**2)))/(1-self.eccentricity * math.cos(math.radians(angle))))
+
+    def centre(self):
+        return (Planet.findr(self,0)-Planet.findr(self,180))/2
+
+    def DRAW(self, win, location):
+        x = (self.x + Planet.centre(self)*Planet.AU) * self.SCALE + WIDTH / 2
         y = self.y * self.SCALE + HEIGHT / 2
-        pygame.draw.circle(win, self.color, (x,y), 3.0)
+        pygame.draw.circle(win, self.color, location, 10.0)
+
+    def get_frames(self):
+        if self.first_four_planets:
+            number_of_frames = ((360 * self.orbital_period) / (2 * math.pi) / data['earth_orbit_time']) * 60
+        else:
+            number_of_frames = ((360 * self.orbital_period) / (2 * math.pi) / data['jupiter_orbit_time']) * 60
+        indexes_of_together_coords = round(1000/number_of_frames)
+        return indexes_of_together_coords
     
+    def ten_rotations_frames(self):
+        if self.first_four_planets:
+            number_of_frames = ((360 * self.orbital_period) / (2 * math.pi) / data['earth_orbit_time']) * 60
+        else:
+            number_of_frames = ((360 * self.orbital_period) / (2 * math.pi) / data['jupiter_orbit_time']) * 60
+
+        return number_of_frames * 10
+
     def create_orbit(self):
         xcoords = []
         ycoords = []
+        line_coordinates = []
+        locations = []
         time_elapsed = 0
         #a thousand degrees to represent each of the 360
-        for i in range (1001):
-            distance = (self.semi_major_axis * (1 - self.eccentricity ** 2)) / (1 - self.eccentricity * math.cos(math.radians((9*i)/25)))
-            xcoords.append(distance * math.cos(math.radians((9*i)/25)))
-            ycoords.append(distance * math.sin(math.radians((9*i)/25)))
-            time_elapsed = (((9*i)/25) * self.orbital_period) / (2 * math.pi)
-        number_of_seconds = time_elapsed/data['earth_orbit_time']
-        print(f'Orbit creation took {number_of_seconds} seconds')
+        for i in range (1000):
+            theta = math.radians((9*i)/25)
+            distance = (self.semi_major_axis * (1 - self.eccentricity ** 2)) / (1 - self.eccentricity * math.cos(theta))
+
+            xcoord = distance * math.cos(theta)
+            ycoord = distance * math.sin(theta)
+            xcoords.append(xcoord)
+            ycoords.append(ycoord)
+
+            line_coordinate = (
+                (xcoord * Planet.AU) * self.SCALE + WIDTH / 2,
+                (ycoord * Planet.AU) * self.SCALE + HEIGHT / 2
+            )
+            line_coordinates.append(line_coordinate)
+
+            if i % Planet.get_frames(self) == 0:
+                locations.append(line_coordinates)
+
         plt.plot(xcoords, ycoords)
+        locations = locations * 60 # so there are enough indexes for the planet to loop
+        return line_coordinates, locations 
 
     def create_orbit_z(self):
         xcoords = []
         ycoords = []
         zcoords = []
-        for i in range (3601):
-            distance = (self.semi_major_axis * (1 - self.eccentricity ** 2)) / (1 - self.eccentricity * math.cos(math.radians(i/10)))
-            xcoords.append(distance * math.cos(math.radians(i/10)) * math.cos(math.radians(self.inclination)))
-            ycoords.append(distance * math.sin(math.radians(i/10)))
-            zcoords.append(distance * math.cos(math.radians(i/10)) * math.sin(math.radians(self.inclination)))
+        for i in range (1000):
+            distance = (self.semi_major_axis * (1 - self.eccentricity ** 2)) / (1 - self.eccentricity * math.cos(math.radians(i*9/25)))
+            xcoords.append(distance * math.cos(math.radians(i*9/25)) * math.cos(math.radians(self.inclination)))
+            ycoords.append(distance * math.sin(math.radians(i*9/25)))
+            zcoords.append(distance * math.cos(math.radians(i*9/25)) * math.sin(math.radians(self.inclination)))
         plt.plot(xcoords, ycoords, zcoords)
+    
+    def match_up_locations(self, other, win, frame):
+        togethercoords_self, locations_self = Planet.create_orbit(self)
+        togethercoords_other, locations_other = Planet.create_orbit(other)
+        pygame.draw.line(win, (0,0,0), locations_self[frame], locations_other[frame], 1)
+        #i += 1    
+
+    def calc_frame_mod(self):
+        change_in_time = 10 * self.orbital_period / 1234
+        return change_in_time * 60
+
+
+    def time_at_certain_angles(self):
+        def simpsons_integration(tuple):
+            d_theta = 1/1000
+            lower, upper = tuple
+
+            lower = lower * convert_1000_to_360
+            upper = upper * convert_1000_to_360
+            #bigger = d_theta / ((1 - self.eccentricity * math.cos(math.radians(upper * convert_1000_to_360))) ** 2)
+            #smaller = d_theta / ((1 - self.eccentricity * math.cos(math.radians(0))) ** 2)
+            
+            N = (upper - lower) / d_theta
+            N = int(N)
+            answer = 0
+            for i in range (0, N + 1):
+                y_of_n = d_theta / (1 - self.eccentricity * math.cos(math.radians(lower + i * d_theta))) ** 2
+                add = y_of_n
+                if i == 0 or i == N:
+                    answer += add
+                elif i % 2 == 0:
+                    answer += add * 2
+                else:
+                    answer += add * 4
+            answer = d_theta / 3 * answer 
+            return answer
+        
+        convert_1000_to_360 = (9/25)
+        '''
+        first_interval = (0, rand(0,1000))
+        second_interval = (first_interval[1], rand(first_interval[1],1000))
+        third_interval = (second_interval[1], 1000)
+        
+        time_first_int = (self.orbital_period * (1 - self.eccentricity ** 2) ** (3/2)) * (1 / (2 * math.pi)) * simpsons_integration(first_interval)
+        '''
+        answer = simpsons_integration((0,1000)) * self.orbital_period * (1 - self.eccentricity ** 2) ** (3/2) * (1 / (2 * math.pi))
+
+        print(answer)
+
+    
+
+            
+
+
+        
+        
 
 
 def find_semi_minor_axis(idx: int) -> float:
@@ -70,6 +165,9 @@ def get_planets() -> List[Planet]:
     """Calculates the semi-minor axis of each planet and returns a list of Planet objects."""
 
     planet_names: List[str] = ['Mercury','Venus','Earth','Mars','Jupiter','Saturn','Uranus','Neptune','Pluto']
+    planet_colors: List[str] = [(128, 128, 128),(245,245,220),(64,224,208),(255,0,0),(255,165,0),(64,224,208),(0,255,0),(0,0,255),(128, 128, 128)]
+    #,'Venus','Earth','Mars','Jupiter','Saturn','Uranus','Neptune','Pluto']
+
     
     planets: List[Planet] = []
     for idx, name in enumerate(planet_names):
@@ -81,7 +179,14 @@ def get_planets() -> List[Planet]:
             data['eccentricity'][idx],
             data['inclination'][idx],
             find_semi_minor_axis(idx),
-            data['semi_major_axis'][idx] * Planet.AU, 0
+            data['semi_major_axis'][idx] * Planet.AU, 
+            0,
+            planet_colors[idx]
         )
+        if idx >= 4:
+            planet.SCALE = 6 / planet.AU
+            planet.first_four_planets = False
+        else:
+            planet.SCALE = 220 / planet.AU
         planets.append(planet)
     return planets
